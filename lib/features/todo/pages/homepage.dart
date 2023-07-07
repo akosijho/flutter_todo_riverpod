70f0e385fe0224950d881f7ec8c0bc8aa9fde09e
akosijho <jhobertpanerio@gmail.com>
2023-07-08 06:36:48 +0800

    Added flutter_screenutil and hooks_riverpod packade.

---
+++ lib/features/todo/pages/homepage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Todo with Rieverpod',
+        style: GoogleFonts.poppins(),),
+      ),
+    );
+  }
+}
