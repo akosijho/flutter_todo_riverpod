7a787270b662d76cba6c065cedd4c6a5cd061cde
akosijho <jhobertpanerio@gmail.com>
2023-07-08 16:59:39 +0800

    Create otp page.

--- lib/features/auth/pages/otp_page.dart
+++ lib/features/auth/pages/otp_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_todo_riverpod/common/utils/constants.dart';
 import 'package:flutter_todo_riverpod/common/widgets/app_style.dart';
 import 'package:flutter_todo_riverpod/common/widgets/gap.dart';
 import 'package:flutter_todo_riverpod/common/widgets/my_text.dart';
+import 'package:pinput/pinput.dart';
 
 class OtpPage extends StatelessWidget {
   const OtpPage({super.key});
@@ -11,6 +12,7 @@ class OtpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
       ),
@@ -38,6 +40,18 @@ class OtpPage extends StatelessWidget {
               const Gap(
                 height: 24,
               ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0.w),
+                child: Pinput(
+                  length: 6,
+                  onCompleted: (value) {
+                    if (value.length == 6) {}
+                  },
+                  onSubmitted: (value) {
+                    if (value.length == 6) {}
+                  },
+                ),
+              )
             ],
           ),
         ),
