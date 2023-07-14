ef84f8306cefe2eebf830a56e877429e36bd1fac
akosijho <jhobertpanerio@gmail.com>
2023-07-14 12:13:25 +0800

    Add update function to tomorrow and day after tomorrow tasks.

--- lib/features/todo/widgets/next_day_task.dart
+++ lib/features/todo/widgets/next_day_task.dart
@@ -5,6 +5,7 @@ import 'package:flutter_todo_riverpod/common/utils/constants.dart';
 import 'package:flutter_todo_riverpod/common/widgets/xpansion_tile.dart';
 import 'package:flutter_todo_riverpod/features/todo/controllers/todo/todo_provider.dart';
 import 'package:flutter_todo_riverpod/features/todo/controllers/xpansion_provider.dart';
+import 'package:flutter_todo_riverpod/features/todo/pages/update_task.dart';
 import 'package:flutter_todo_riverpod/features/todo/widgets/todo_tile.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 
@@ -53,7 +54,11 @@ class NextDayTasks extends ConsumerWidget {
                 await ref.read(todoStateProvider.notifier).deleteTask(e.id ?? 0);
               },
               editWidget: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context){
+                    return UpdateTask(e);
+                  }));
+                },
                 icon: const Icon(MaterialCommunityIcons.circle_edit_outline,
                   color: Colors.white,),
               ),
