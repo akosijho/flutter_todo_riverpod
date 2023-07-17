3e4f94f10035cc0f64199ad2923de754afcab44d
akosijho <jhobertpanerio@gmail.com>
2023-07-17 15:26:21 +0800

    Verify otp.

--- lib/features/auth/pages/otp_page.dart
+++ lib/features/auth/pages/otp_page.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_todo_riverpod/common/utils/constants.dart';
 import 'package:flutter_todo_riverpod/common/widgets/app_style.dart';
 import 'package:flutter_todo_riverpod/common/widgets/gap.dart';
 import 'package:flutter_todo_riverpod/common/widgets/my_text.dart';
+import 'package:flutter_todo_riverpod/features/auth/controllers/auth_controller.dart';
 import 'package:pinput/pinput.dart';
 
-class OtpPage extends StatelessWidget {
+class OtpPage extends ConsumerWidget {
   const OtpPage({super.key, required this.smsCodeId, required this.phone});
 
   final String smsCodeId;
   final String phone;
 
+  verifyOtp(BuildContext context, WidgetRef ref, String smsCode) {
+    ref
+        .read(authControllerProvider)
+        .verifyOtpCode(context: context, smsCodeID: smsCodeId, smsCode: smsCode, mounted: true);
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
@@ -48,10 +56,14 @@ class OtpPage extends StatelessWidget {
                 child: Pinput(
                   length: 6,
                   onCompleted: (value) {
-                    if (value.length == 6) {}
+                    if (value.length == 6) {
+                      verifyOtp(context, ref, value);
+                    }
                   },
                   onSubmitted: (value) {
-                    if (value.length == 6) {}
+                    if (value.length == 6) {
+                      verifyOtp(context, ref, value);
+                    }
                   },
                 ),
               )
